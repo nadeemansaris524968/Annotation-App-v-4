0f2d36e436c11837dbf99184ac32edde18881585
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2018-01-26 16:11:16 -0800

    Remove separate method for attributeDiv.
    Changed setupAnatomy to getAutocompleteOptions to return only options.

--- index.js
+++ index.js
@@ -5890,18 +5890,45 @@ var AnnotationCtrl = (function() {
 
 /* UIController */
 var UICtrl = (function() {
+  var getAutocompleteOptions = function(searchData) {
+    // Options for EasyAutocomplete
+    var options = { 
+      data: searchData, 
+      listLocation: function(element) {
+        return element["Major Anatomic Region"];
+      }, 
+      getValue: "name", 
+      list: { 
+        match: { enabled: true }, 
+        maxNumberOfElements: 50, 
+        onChooseEvent: function() {
+          var majorAnatomy = $(DOMStrings.anatomy).val();
+
+          searchData["Major Anatomic Region"].find(function(element) {
+            if (element.name === majorAnatomy) {
+              var findings = element["Findings"];
+              UICtrl.setupFindings(findings);
+            }
+          });
+        }
+      }, 
+      theme: "square"
+    };
+
+    return options;
+  };
+
   return {
-    createAttributeDiv: function(attribute, parentDivID) {
+    createAttributeDiv: function(attribute, parentDivID, searchData) {
       // AttributeDiv
-      var attributeDiv = document.createElement("div");
-      attributeDiv.setAttribute("class", `${parentDivID} divAttribute`);
-      attributeDiv.setAttribute("id", attribute.Key);
+      // var attributeDiv = document.createElement("div");
+      // attributeDiv.setAttribute("class", `${parentDivID} divAttribute`);
 
       // Heading inside AttributeDiv
-      var attributeHeading = document.createElement("div");
-      attributeHeading.style.fontSize = "8px";
-      attributeHeading.style.color = "#c14343";
-      attributeHeading.textContent = attribute.Key;
+      // var attributeHeading = document.createElement("div");
+      // attributeHeading.style.fontSize = "8px";
+      // attributeHeading.style.color = "#c14343";
+      // attributeHeading.textContent = attribute.Key;
 
       // Edit Pencil inside AttributeDiv
       // var editPencil = document.createElement('span');
@@ -5910,27 +5937,34 @@ var UICtrl = (function() {
       // editPencil.style.cssFloat = 'right';
 
       // Autocomplete Input field inside AttributeDiv
-      var attributeInput = document.createElement("input");
-      attributeInput.setAttribute("type", "text");
-      attributeInput.setAttribute("value", attribute.Value);
-      attributeInput.style.fontSize = "10px";
-      attributeInput.style.marginTop = "3px";
+      // var attributeInput = document.createElement("input");
+      // attributeInput.setAttribute("type", "text");
+      // attributeInput.setAttribute("class", parentDivID);
+      // attributeInput.setAttribute("id", attribute.Key);      
+      // attributeInput.setAttribute("value", attribute.Value);
+      // attributeInput.style.fontSize = "10px";
+      // attributeInput.style.marginTop = "3px";
 
-      // attributeDiv.appendChild(editPencil);
-      attributeDiv.appendChild(attributeHeading);
-      attributeDiv.appendChild(attributeInput);
+      // if (attribute.Key === 'Major Anatomic Region') {
+      //   setupAnatomy(searchData, '');
+      // }
 
-      return attributeDiv;
+      // // attributeDiv.appendChild(editPencil);
+      // attributeDiv.appendChild(attributeHeading);
+      // attributeDiv.appendChild(attributeInput);
+
+      // return attributeDiv;
     },
 
-    createFindingDiv: function(findingRows) {
+    createFindingDiv: function(findingRows, searchData) {
       if (findingRows.length > 0) {
+        // Create FindingsDiv
         for (var i = 0; i < findingRows.length; i++) {
           var findingsDiv = document.createElement("div");
-          var findingsRowID = "finding_row_" + i;
+          var findingsRowClass = "finding_row_" + i;
           findingsDiv.setAttribute(
             "class",
-            `alert alert-info alert-dismissable ${findingsRowID}`
+            `alert alert-info alert-dismissable ${findingsRowClass}`
           );
           findingsDiv.style.overflow = "hidden";
 
@@ -5943,12 +5977,35 @@ var UICtrl = (function() {
 
           findingsDiv.appendChild(closeFindingsAnchor);
 
+          // Create attributeDivs
           for (var j = 0; j < findingRows[i]["row"].length; j++) {
-            var attributeKeyValue = findingRows[i]["row"][j];
-            var attributeDiv = UICtrl.createAttributeDiv(
-              attributeKeyValue,
-              findingsRowID
-            );
+            var attribute = findingRows[i]["row"][j];
+
+            var attributeDiv = document.createElement('div');
+            attributeDiv.setAttribute("class", `${findingsRowClass} divAttribute`);
+
+            var attributeHeading = document.createElement("div");
+            attributeHeading.style.fontSize = "8px";
+            attributeHeading.style.color = "#c14343";
+            attributeHeading.textContent = attribute.Key;
+
+            var attributeInput = document.createElement("input");
+            attributeInput.setAttribute("type", "text");
+            attributeInput.setAttribute("class", `${findingsRowClass}`);
+            attributeInput.setAttribute("id", `${j}`);
+            attributeInput.setAttribute("value", attribute.Value);
+            attributeInput.style.fontSize = "10px";
+            attributeInput.style.marginTop = "3px";
+
+            var inputSelector = '#' + attributeInput.id + '.' + attributeInput.className;
+            console.log('Input selector: ', inputSelector);
+            // Setup anatomy input
+            var options = getAutocompleteOptions(searchData);
+            $('input').easyAutocomplete(options);
+
+            attributeDiv.appendChild(attributeHeading);
+            attributeDiv.appendChild(attributeInput);
+
             findingsDiv.appendChild(attributeDiv);
           }
 
@@ -5970,7 +6027,9 @@ var controller = (function() {
       var cxr_key = "152ba901-12f8-44aa-a60e-c5217e1e9497";
       var findingRows = AnnotationCtrl.getRows(cxr_key);
 
-      UICtrl.createFindingDiv(findingRows);
+      var data = SearchCtrl.getSearchData();
+
+      UICtrl.createFindingDiv(findingRows, data);
     }
   };
 })(UICtrl, AnnotationCtrl, SearchCtrl);
